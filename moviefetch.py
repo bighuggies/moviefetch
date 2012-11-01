a647328f3cd02b87d58a8104081b26af718975e9
Andrew Hughson <andrew@hughson.me>
2012-11-02 04:14:35 +1300

    Move some code around

--- moviefetch.py
+++ moviefetch.py
@@ -11,31 +11,48 @@ import requests
 endwith = re.compile('.*[0-9]\)$')  # matches a string ending in [0-9])
 
 
-def sanitise_title(title):
-    if '.' in title:
-        title = ' '.join(title.split('.')[:-1])
+def rename_movie(path, title, year):
+    titleandyear = '{title} ({year})'.format(
+        title=title, year=year)
+    titleandyear = str.replace(titleandyear, ': ', ' - ')
 
-    if '[' in title:
-        title = ' '.join(title.split('[')[:-1])
+    if input('Is "{new}" a good folder name? (y/n): '.format(
+             new=titleandyear)).lower() == 'y':
 
-    return title.strip()
+        print('Renaming "{old}" to "{new}"'.format(
+            old=path, new=titleandyear))
+
+        try:
+            logging.info('Renaming "{old}" to "{new}"'.format(
+                old=path, new=titleandyear))
+            os.rename(
+                path, os.path.join(path, titleandyear))
+        except:
+            print('Skipping "{movie}" (failed to rename).'.format(
+                movie=title))
+            logging.error('Renaming "{old}" to "{new}" failed'.format(
+                old=path, new=titleandyear))
+
+        return True
+
+    return False
 
 
 def tidy_movies(path):
     for filename in os.listdir(path):
         # Full path is useful for manipulating directories
         full_path = os.path.join(path, filename)
-
-        # Get rid of file extensions and dates formatted like [2001]
-        title = sanitise_title(filename)
+        title = filename.strip()
 
         # If the string ends with [0-9]) we assume it already has a date
         if endwith.match(title):
             print('Skipping "{movie}" (already has year).'.format(movie=title))
-            break
+            continue
 
         # If the entry is a file, move it into a directory
         if os.path.isfile(full_path):
+            # Get rid of file extension
+            title = os.path.splitext(title)[0]
             new_path = os.path.join(path, title)
 
             print('Moving {movie} into a directory.'.format(movie=title))
@@ -52,36 +69,16 @@ def tidy_movies(path):
         print('Getting data for "{movie}"'.format(movie=title))
         data = json.loads(requests.get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=xrwz8fmkszafbkgyzn2xbegz&q={title}&page_limit=3&page=1'.format(title=title)).text)
 
-        if 'movies' not in data or data['total'] <= 0:
+        if 'movies' in data and data['total'] > 0:
+            for movie in data['movies']:
+                # If we find a good title, don't look at the rest
+                if rename_movie(full_path, movie['title'], str(movie['year'])):
+                    break
+        else:
             logging.debug(
                 'Skipping "{movie}" (no data found).'.format(movie=title))
-        else:
-            for movie in data['movies']:
-                titleandyear = '{title} ({year})'.format(
-                    title=movie['title'], year=str(movie['year']))
-                titleandyear = str.replace(titleandyear, ': ', ' - ')
 
-                print('Is "{new}" a good folder name? (y/n): '.format(
-                    new=titleandyear))
 
-                if input() == 'y':
-                    print('Renaming "{old}" to "{new}"'.format(
-                        old=full_path, new=titleandyear))
-
-                    try:
-                        logging.info('Renaming "{old}" to "{new}"'.format(
-                            old=full_path, new=titleandyear))
-                        os.rename(
-                            full_path, os.path.join(path, titleandyear))
-                    except:
-                        print('Skipping "{movie}" (failed to rename).'.format(
-                            movie=title))
-                        logging.error('Renaming "{old}" to "{new}" failed'.format(
-                            old=full_path, new=titleandyear))
-                        break
-
-                    break
-
-if __name__ == '__tidy_movies__':
+if __name__ == '__main__':
     logging.basicConfig(filename='moviefetch.log', level=logging.DEBUG)
     tidy_movies(sys.argv[1])
