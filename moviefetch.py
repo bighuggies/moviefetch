9b6a04bf0bfb8e9cff80b0573fca6384f832234c
Andrew Hughson <andrew@hughson.me>
2014-12-19 21:47:38 +1300

    Take out API key

--- moviefetch.py
+++ moviefetch.py
@@ -32,7 +32,7 @@ def rename_movie(path, title, year):
     return False
 
 
-def tidy_movies(path):
+def tidy_movies(path, api_key):
     os.chdir(path)
 
     for filename in os.listdir(path):
@@ -45,27 +45,11 @@ def tidy_movies(path):
             print('Skipping "{movie}" (already has year).'.format(movie=title))
             continue
 
-        # If the entry is a file, move it into a directory
-        if os.path.isfile(full_path):
-            # Get rid of file extension
-            title = os.path.splitext(title)[0]
-            new_path = os.path.join(path, title)
-
-            print('Moving {movie} into a directory.'.format(movie=title))
-            logging.info('Moving "{title}" from "{source}" to "{dest}"\n'.format(
-                title=title, source=full_path, dest=new_path))
-
-            os.mkdir(new_path)
-            shutil.move(full_path, new_path)
-
-            # Change the full path to the new location
-            full_path = new_path
-
-        # Get three likely title/dates by searching the title on rotten
-        # tomatoes
+        # Get likely title/dates by searching the title on rotten tomatoes
         print('Getting data for "{movie}"'.format(movie=title))
         data = json.loads(
-            requests.get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=xrwz8fmkszafbkgyzn2xbegz&q={title}&page_limit=10&page=1'.format(title=title)).text)
+            requests.get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey={api_key}&q={title}&page_limit=10&page=1'.format(api_key=api_key, title=title)).text)
+        logging.debug('Got data: {data}'.format(data=data))
 
         if 'movies' in data and data['total'] > 0:
             for movie in data['movies']:
@@ -78,5 +62,7 @@ def tidy_movies(path):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='moviefetch.log', level=logging.DEBUG)
-    tidy_movies(sys.argv[1])
+    with open('config.json') as cfg:
+        cfg = json.load(cfg)
+        logging.basicConfig(filename='moviefetch.log', level=logging.DEBUG)
+        tidy_movies(sys.argv[1], cfg['apiKey'])
